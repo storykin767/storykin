f71959c8bc4bad140cb310979996f43c7e74bf92
Bhatti <A3014443@bestbuy.com>
2026-04-04 16:45:43 -0500

    Sprint 6 - Sentry monitoring live

---
+++ frontend/sentry.server.config.ts
@@ -0,0 +1,16 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://bd8a1f741e501f84e7cbf1259217d007@o4511163852062720.ingest.us.sentry.io/4511163859992576",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
